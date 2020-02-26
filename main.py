4ff05b4d5a5892405f2870b7a4c10660868d4c9a
Joel <17141@burnside.school.nz>
2020-02-26 22:30:55 +1300

    V3.1 Fully Fucntioning Create/View Of Notices With Mininmal Css.

--- main.py
+++ main.py
@@ -48,25 +48,9 @@ def home():
     if logstatus == "true":
         conn = sqlite3.connect('users.db')
         c = conn.cursor()
-        c.execute("SELECT user, post FROM notices")
+        c.execute("SELECT name, post FROM notices")
         posts = c.fetchall()
         conn.close()
-        # users = [i[0] for i in posts]
-        # notices = [b[1] for b in posts]
-        # conn = sqlite3.connect('users.db')
-        # user_names = []
-        # user_new = []
-        # c = conn.cursor()
-        # for i in range (len(users)):
-        #     c.execute("SELECT user FROM users WHERE id=?", (users[i],))
-        #     a = c.fetchall()
-        #     user_names.insert(i, a[0])
-        # print(user_names)
-        # final_post = []
-        # for i in range (len(user_names)):
-        #     post_temp = (user_names[i], posts[i, 1])
-        #     # final_post.insert(i, post_temp)
-        #     print(post_temp)
         if xadmin == "Admin":
             return render_template("home.html", admin = "Admin", posts = posts)
         else:
@@ -92,15 +76,19 @@ def post_admin():
     post_db = []
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
-    # c.execute("SELECT id from users WHERE user=?", (session.get('user', None),))
-    # for i in c.fetchall():
-    #     for j in i:
-    #         user_id = j
     user_id = session.get('user', None)
+    c.execute("SELECT name FROM users WHERE user=?", (user_id,))
+    for i in c.fetchall():
+        for j in i:
+            name = j
+    conn.close()
     post_db.insert(1, user_id)
     post_db.insert(2, request.form['post'])
-    sql = "INSERT INTO notices(user, post) VALUES(?,?)"
+    post_db.insert(3, name)
+    sql = "INSERT INTO notices(user, post, name) VALUES(?,?,?)"
     val = post_db
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
     c.execute(sql, val)
     conn.commit()
     conn.close()

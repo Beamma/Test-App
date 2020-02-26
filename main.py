1aabebe8cc6fe78397df1f65acbc04a633f4c725
Joel <17141@burnside.school.nz>
2020-02-26 21:37:04 +1300

    V3.1 Near Completed Notices Without Css.

--- main.py
+++ main.py
@@ -44,14 +44,29 @@ def home():
     logstatus = 'false'
     xadmin = session.get('xadmin', None)
     logstatus = session.get('logstatus', None)
+    users = []
     if logstatus == "true":
         conn = sqlite3.connect('users.db')
         c = conn.cursor()
         c.execute("SELECT user, post FROM notices")
         posts = c.fetchall()
-        print(posts)
         conn.close()
-
+        # users = [i[0] for i in posts]
+        # notices = [b[1] for b in posts]
+        # conn = sqlite3.connect('users.db')
+        # user_names = []
+        # user_new = []
+        # c = conn.cursor()
+        # for i in range (len(users)):
+        #     c.execute("SELECT user FROM users WHERE id=?", (users[i],))
+        #     a = c.fetchall()
+        #     user_names.insert(i, a[0])
+        # print(user_names)
+        # final_post = []
+        # for i in range (len(user_names)):
+        #     post_temp = (user_names[i], posts[i, 1])
+        #     # final_post.insert(i, post_temp)
+        #     print(post_temp)
         if xadmin == "Admin":
             return render_template("home.html", admin = "Admin", posts = posts)
         else:
@@ -77,10 +92,11 @@ def post_admin():
     post_db = []
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
-    c.execute("SELECT id from users WHERE user=?", (session.get('user', None),))
-    for i in c.fetchall():
-        for j in i:
-            user_id = j
+    # c.execute("SELECT id from users WHERE user=?", (session.get('user', None),))
+    # for i in c.fetchall():
+    #     for j in i:
+    #         user_id = j
+    user_id = session.get('user', None)
     post_db.insert(1, user_id)
     post_db.insert(2, request.form['post'])
     sql = "INSERT INTO notices(user, post) VALUES(?,?)"
